cbd8954d5276089c0061c52d8acc2e5d168b5e77
K Chuprin <netvorenje@yandex.kz>
2022-10-24 21:32:41 +0600

    fix: /review1: изменить js логику открытия-закрытия попап и события сохранения

--- scripts/index.js
+++ scripts/index.js
@@ -9,22 +9,21 @@ let form = popup.querySelector('.input');
 let popupUserName = popup.querySelector('.input__name');
 let popupUserAbout = popup.querySelector('.input__occupation');
 
-// автозаполнение формы
-function autocompletePopupForms() {
-  userName.textContent = popupUserName.value;
-  userAbout.textContent = popupUserAbout.value;
-};
 
 // открыть попап
 const popupOpenEvent = function () {
   popup.classList.add('popup_opened');
-  autocompletePopupForms();
-  // при открытии попапа предполагается, что форма автозаполняется
+  // вставить данные при открытии
+  popupUserName.value = userName.textContent; 
+  popupUserAbout.value = userAbout.textContent;
 };
 
 // закрыть попап
 const popupCloseEvent = function () {
   popup.classList.remove('popup_opened');
+  // при нажатии на крестик не сохранять введенные значения
+  popupUserName.value = '';
+  popupUserAbout.value = '';
 };
 
 // слушатели событий - по клику на кнопку редактировать или крестик
@@ -32,20 +31,15 @@ popupOpenButton.addEventListener('click', popupOpenEvent);
 popupCloseButton.addEventListener('click', popupCloseEvent);
 
 // изменить информацию о пользователе
-
 function editUserInfo (evt) {
   evt.preventDefault();
-  autocompletePopupForms();
+  userName.textContent = popupUserName.value;
+  userAbout.textContent = popupUserAbout.value;
 
-  popupCloseEvent();
+  popup.classList.remove('popup_opened');
+  // или вызов функции
+  // popupCloseEvent();
 };
 
 // слушатель нужно навешивать на тег form
 form.addEventListener('submit', editUserInfo);
-
-
-// НЕ РАБОТАЕТ:
-// 1. При первом открытии форма не заполнена
-// 2. При первом закрытии незаполненные поля формы "обнуляют" значения или сохраняют пустые
-// 3. При закрытии через крестик и новом открытии несохраненные данные в форме остаются
-// 4. При последующих открытиях форма зааполена последними введенными данными, не данными со страницы
